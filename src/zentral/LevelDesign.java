aeb378fd89ba074ed1705fbace63700b9b792858
Ines Rohrbach <ines.rohrbach@tud.htwk-leipzig.de>
2021-12-11 13:34:42 +0100

    interfaces erstellt und Leveldesign fuer phase2 denke ich fertig

--- src/zentral/LevelDesign.java
+++ src/zentral/LevelDesign.java
@@ -1,12 +1,40 @@
 package zentral;
 
 /**
- * Klasse, welche die fuer das LEvel notwenigen Spielelemente erstellt.
+ * Klasse, welche die fuer das Level notwenigen Spielelemente erstellt.
  * Die Level sind vorgefertit in Textform vorhanden.
  * 
  * @author iveev
  */
 public class LevelDesign {
-	public LevelDesign(uint)
+	private Set<IFSpielemlement> alleElemente = new Hashset<IFSpielelement>();
+	
+	/**
+	 * Konstruktor fuer ein Leveldesign, welches je nach gewaehltem Level und Schwierigkeit das gewuenschte Level erstellt.
+	 * 
+	 * 
+	 * @param level, Fassanrichtung
+	 * @param schwierigkeit, Fassnummern und Eventfaesser
+	 * @author iveev
+	 */
+	public LevelDesign(byte level, byte schwierigkeit) {		
+	}
+	
+	/**
+	 * Liesst die ausgewaehlte Textdatei ein und erstellt die Elemente ueber die Factory.
+	 * 
+	 * @param dateiname, Name des genauen Levels 
+	 * @author iveev
+	 */
+	private void leseDatei(String dateiname) {	
+	}
 
+	/**
+	 * Gibt alle für das Level verwendeten Spielelemente zurueck
+	 * 
+	 * @return alle Spielelmente des Levels in einem Hashset
+	 * @author iveev
+	 */
+	public Set<IFSpielelement> getElemente() {
+	}
 }
