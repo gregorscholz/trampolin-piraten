bb250802d6c7469b2e2a7d8c5a497e6cee46cd98
gscholz <gregor_walter.scholz@stud.htwk-leipzig.de>
2021-12-14 16:24:52 +0000

    Resourceloader implementiert

--- src/gui/WindowPanel.java
+++ src/gui/WindowPanel.java
@@ -1,10 +1,6 @@
 package gui;
 import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 /**
@@ -15,36 +11,21 @@ import javax.swing.JPanel;
 public class WindowPanel extends JPanel {
 
     static GamePanel gp;
-    private BufferedImage bi;
 
     public WindowPanel() {
         gp = new GamePanel();
-        getBackgroundImage();
         this.add(gp);
     }
 
     /**
-     * lädt das Hintergrundbild in das Programm
-     * 
-     * @author Gregor Scholz
-     */
-    public void getBackgroundImage() {
-        try {
-            bi = ImageIO.read(new File("resources/background.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * zeichnet das Hintergrundbild auf das Panel
+     * Zeichnet das Hintergrundbild auf das Panel
      * 
      * @author Gregor Scholz
      */
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);           
-        g.drawImage(bi, 0, 0, null);
+        g.drawImage(ResourceLoader.getBackground(), 0, 0, null);
     }
     
 }
