1c1744eb93e4f511d2d83aacfc9019abc4b846d2
Johannes <Johannes@192.168.0.6>
2021-12-15 23:40:20 +0100

    Kugel und Plattform Klasse hinzugefügt (noch zum Groß unkommentiert)

---
+++ src/spielelemente/Kugel.java
@@ -0,0 +1,63 @@
+package spielelemente;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Kugel extends Rectangle {
+
+	static final int BALL_DIAMETER;
+	int xVelocity;
+	int yVelocity;
+	int xKoordinate, yKoordinate;
+
+
+	Kugel(){
+        //Initialkoordinaten als Parameter, damit Ballmultiplikatorevent am Block Baelle erstellen kann
+        //Initialgeschwindigkeit
+        //Größe (Durchmesser)
+	}
+	
+	//Getter und Setter
+	
+	public void setxVelocity(int newxVelocity){
+        xVelocity = x;
+    }
+
+    public void setyVelocity(int newyVelocity){
+        yVelocity = newyVelocity;
+    }
+
+	public int getxVelocity() {
+		return xVelocity;
+	}
+
+	public int getyVelocity() {
+		return yVelocity;
+	}
+
+	public int getxKoordinate() {
+		return xKoordinate;
+	}
+
+	public void setxKoordinate(int xKoordinate) {
+		this.xKoordinate = xKoordinate;
+	}
+
+	public int getyKoordinate() {
+		return yKoordinate;
+	}
+
+	public void setyKoordinate(int yKoordinate) {
+		this.yKoordinate = yKoordinate;
+	}
+	
+	//andere Methoden
+	
+	public void move() {
+		
+	}
+	
+	public void draw(Graphics g) {
+		//malt die Kugel; wird wahrscheinlich von Gregor in der GUI uebernommen
+	}
+}
---
+++ src/spielelemente/Plattform.java
@@ -0,0 +1,72 @@
+package spielelemente;
+
+import java.awt.event.*;
+import java.awt.*;
+
+public class Plattform extends Rectangle {
+
+	int PlattformBreite;
+	int PlattformGroesse;
+	int xVelocity;
+	int xKoordinate, yKoordinate;
+	
+	
+	Plattform(){
+		//Initialkoordinaten
+		//Groesse
+		//Geschwindigkeit in x-richtung (0), aendert sich wenn Plattform bewegt wird
+	}
+	
+	//Getter und Setter
+		
+	public int getPlattformBreite() {
+		return PlattformBreite;
+	}
+
+	public void setPlattformBreite(int plattformBreite) {
+		PlattformBreite = plattformBreite;
+	}
+
+	public int getPlattformGroesse() {
+		return PlattformGroesse;
+	}
+
+	public void setPlattformGroesse(int plattformGroesse) {
+		PlattformGroesse = plattformGroesse;
+	}
+
+	public int getxVelocity() {
+		return xVelocity;
+	}
+
+	public void setxVelocity(int xVelocity) {
+		this.xVelocity = xVelocity;
+	}
+
+	public int getxKoordinate() {
+		return xKoordinate;
+	}
+
+	public void setxKoordinate(int xKoordinate) {
+		this.xKoordinate = xKoordinate;
+	}
+
+	public int getyKoordinate() {
+		return yKoordinate;
+	}
+
+	public void setyKoordinate(int yKoordinate) {
+		this.yKoordinate = yKoordinate;
+	}
+
+	
+	//andere Methoden
+	
+	public void move() {
+		
+	}
+	
+	public void draw(Graphics g) {
+		//malt die Plattform; wird wahrscheinlich von Gregor in der GUI uebernommen
+	}
+}
