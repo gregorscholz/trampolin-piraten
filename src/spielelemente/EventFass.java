1ac4b3a5c60c4242350d3886051b393c85ab407d
Inga <Inga@LAPTOP-T8PDFS14.speedport.ip>
2021-12-16 13:16:46 +0100

    hoffendlich klappts

---
+++ src/spielelemente/EventFass.java
@@ -0,0 +1,40 @@
+package spielelemente;
+
+public class EventFass extends Fass {
+
+	/**
+	 * Verwalten und Auslösen der Events
+	 * @param breite, Anzahl der möglichen Nebeneinanderliegenden Fässer
+	 * @param betroffen, Fass welches von Events mit betroffen ist aber NICHT das aufgerufene Eventfass
+	 * @param symbol, zeichen des Eventes
+	 * @author ischramm
+	 */
+	
+	byte symbol; // als Array 0 =kein Event {1,2,3,4 = Event}
+	Fass betroffen;
+	int breite = 25;
+
+	public EventFass(int fassNummer, int eventnummer) {
+		super(eventnummer);
+			switch(eventnummer) {
+			case 1: //Schwarzpulver
+				betroffen = new Fass(fassNummer- (breite +1),0,0);
+				betroffen = new Fass(fassNummer- (breite),0,0);
+				betroffen = new Fass(fassNummer- (breite-1),0,0);
+				betroffen = new Fass(fassNummer- (1),0,0);
+				betroffen = new Fass(fassNummer+ (1),0,0);
+				betroffen = new Fass(fassNummer+ (breite -1),0,0);
+				betroffen = new Fass(fassNummer+ (breite),0,0);
+				betroffen = new Fass(fassNummer+ (breite +1),0,0);
+			case 2: //Munition: for(int i = 0, i<7, i++) Ball.bewegung = position[this];
+			case 3: // Rum: Bewegungssteuerung.links = zwischenspeicher;
+					//Bewegungssteuerung.links = Bewegungssteuerung.rechts;
+					// Bewegungssteuerung.rechts = zwischenspeicher;
+					//TimeUnit.SECONDS.sleep(5);
+					//Bewegungssteuerung.links = zwischenspeicher;
+					//Bewegungssteuerung.links = Bewegungssteuerung.rechts;
+					// Bewegungssteuerung.rechts = zwischenspeicher;
+			case 4: //Wind: Unterer Rand = unterer Rand - x Pixel
+			}
+	}
+}
---
+++ src/spielelemente/Fass.java
@@ -0,0 +1,67 @@
+package spielelemente;
+
+import gui.InGamePanel;
+
+/**
+ * Klasse zum Verwalten der Fässer
+ * @param verbleibend, Anzahl der verbleibenden treffer bis zum verschwinden eines Fasses
+ * @param Fassposition, Array zum verwalten der Fässer
+ * @param KoordinateX, x position eines Fasses (oben links)
+ * @param KoordinateY, y position eines Fasses (oben links)
+ * @author ischramm
+ */
+
+public class Fass {
+
+	int verbleibend;
+	int KoordinateX;
+	int KoordinateY;
+	
+   /**
+     *Konstruktor zu Collisionsbehandlung
+     *@author ischramm
+     * @return Anzahl der verbleibenden "lebens"punke
+     */
+	public Fass(int fassNummer, int leben, int eventnummer) {
+			leben = leben-1;
+			verbleibend = leben;
+			if (eventnummer != 0) {
+				EventFass auslöser = new EventFass(fassNummer, eventnummer);
+			}
+			
+	}
+	
+	   /**
+     *Konstruktor zum erstellen der Fässer (positionsbehandlung)
+     *@author ischramm
+     */
+	public Fass(int fassNummer) {
+	//InGamePanel.paintfass (KoordinateX, KoordinateY); oder so
+	}
+
+	 /**
+     * 3 getter zum aubfragen der:
+     * @getLeben , Anzahl der verbleibenden treffer bis zum verschwinden eines Fasses
+     * @getX , x position eines Fasses (oben links)
+     * @getY , y position eines Fasses (oben links)
+     * @autor ischramm
+     */
+	public int getLeben(){
+		return verbleibend;
+	}
+	public int getX() {
+		return KoordinateX;
+	}
+	public int getY() {
+		return KoordinateY;
+	}
+	
+	 /**
+     * Berechnen der Position 
+     * @param fassnummer, position des Fasses im Array
+     * @autor ischramm
+     */
+	public void berechnePosition(int fassnummer) {
+		//berechnet x und y Koordinaten eines Fasses
+	}
+}
