fca17cf2f6d50e79b1c896e24c31d9cb63f4dfe5
fseiffer <ferdinand.seiffert@stud.htwk-leipzig.de>
2022-01-10 20:01:17 +0100

    Vektorberechnung mit Hilfselementen und Baustellen

--- src/spielelemente/CollisionDetection.java
+++ src/spielelemente/CollisionDetection.java
@@ -1,6 +1,7 @@
 package spielelemente;
 
 import java.awt.Rectangle;
+import java.awt.geom.Point2D;
 
 import gui.InGamePanel;
 
@@ -47,10 +48,12 @@ public class CollisionDetection{
     */
     private boolean checkFaesser(){
         for(Fass i : gameObjects.getFaesser()){
-            if(gameObjects.getKugel().intersects(i){
-                i.triggerEvent();
-                gameObjects.getKugel().setxVelocity(calcVector(gameObjects.getKugel(), i)[0]);
-                gameObjects.getKugel().setyVelocity(calcVector(gameObjects.getKugel(), i)[1]);
+            if(i != null){
+                if(gameObjects.getKugel().intersects(i)){
+                    i.treffer();
+                    gameObjects.getKugel().setxVelocity(calcVector(i)[0]);
+                    gameObjects.getKugel().setyVelocity(calcVector(i)[1]);
+                }
             }
         }
         return false;
@@ -63,9 +66,11 @@ public class CollisionDetection{
     */
     private boolean checkPlattform(){
         if(gameObjects.getKugel().intersects(gameObjects.getPlattform())){
-            gameObjects.getKugel().setxVelocity(calcVector(gameObjects.getKugel(), gameObjects.getPlattform())[0]);
-            gameObjects.getKugel().setyVelocity(calcVector(gameObjects.getKugel(), gameObjects.getPlattform())[1]);
+            gameObjects.getKugel().setxVelocity(calcVector(gameObjects.getPlattform())[0]);
+            gameObjects.getKugel().setyVelocity(calcVector(gameObjects.getPlattform())[1]);
+            return true;
         }
+        return false;
     }
     
     /**
@@ -85,7 +90,7 @@ public class CollisionDetection{
             return true;
         }
         //Kugel trifft unten
-        if(gameObjects.getKugel().getyKoordinate() < gameObjects.getPlattform().y){
+        if(gameObjects.getKugel().getyKoordinate() < gameObjects.getPlattform().getMinY()){
             controller.resetKugel();//TODO
             return true;
         }
@@ -99,6 +104,77 @@ public class CollisionDetection{
      *@param coll Objekt, mit dem Kugel kollidiert
     */
     private double[] calcVector(Rectangle coll){
-        return null;
+        boolean xVorz;
+        boolean yVorz;
+        if(gameObjects.getKugel().getxVelocity()<0){xVorz = false;}
+        else if(gameObjects.getKugel().getxVelocity()>0){xVorz = true;}
+        else{return new double[]{0.0, -gameObjects.getKugel().getyVelocity()};}
+        if(gameObjects.getKugel().getxVelocity()<0){yVorz = false;}
+        else if(gameObjects.getKugel().getxVelocity()>0){yVorz = true;}
+        else{return new double[]{-gameObjects.getKugel().getxVelocity(), 0.0};}
+        boolean oberhalb = eckUnterscheidung(coll, xVorz, yVorz);
+        boolean flip = false;//TODO flip berechnen
+        if(flip){return new double[]{-gameObjects.getKugel().getxVelocity(), gameObjects.getKugel().getxVelocity()};}
+        else{return new double[]{gameObjects.getKugel().getxVelocity(), -gameObjects.getKugel().getxVelocity()};}
+    }
+
+    /**
+     * Hilfsfunktion zur Bestimmung der Variable "oberhalb" in this.calcVerctor(Rectangle)
+     *@author fseiffer
+     *@return ob kugel oberhalb oder unterhalb von "collEcke" kollidiert
+     *@param coll Objekt, mit dem Kugel kollidiert
+     *@param xVorz xRichtung des Bewegungsvectors
+     *@param yVorz yRichtung des Bewegungsvectors
+    */
+    private boolean eckUnterscheidung(Rectangle coll, boolean xVorz, boolean yVorz){
+        Gerade g;
+        Point2D.Double collEcke;
+        Point2D.Double vec = new Point2D.Double(gameObjects.getKugel().getxVelocity(),gameObjects.getKugel().getyVelocity());
+        if(xVorz && yVorz){
+            g = new Gerade(new Point2D.Double(gameObjects.getKugel().getMaxX(), gameObjects.getKugel().getMaxY()), vec);
+            collEcke = new Point2D.Double(coll.getMinX(), coll.getMinY());
+        }
+        else if(!xVorz && !yVorz){
+            g = new Gerade(new Point2D.Double(gameObjects.getKugel().getMinX(), gameObjects.getKugel().getMinY()), vec);
+            collEcke = new Point2D.Double(coll.getMaxX(), coll.getMaxY());
+        }
+        else if(!xVorz && yVorz){
+            g = new Gerade(new Point2D.Double(gameObjects.getKugel().getMinX(), gameObjects.getKugel().getMaxY()), vec);
+            collEcke = new Point2D.Double(coll.getMaxX(), coll.getMinY());
+        }
+        else{
+            g = new Gerade(new Point2D.Double(gameObjects.getKugel().getMaxX(), gameObjects.getKugel().getY()), vec);
+            collEcke = new Point2D.Double(coll.getMinX(), coll.getMaxY());
+        }
+        return g.istOberhalb(collEcke);
+    }
+
+    /**
+    * Hilfsklasse zur Darstellungen von Geraden 
+    * @author fseiffer
+    */
+
+    private class Gerade{
+        private double m;
+        private double t;
+
+        public Gerade(Point2D.Double p, Point2D.Double vector){//TODO Grenzfaelle beachten
+            m = vector.getX()/vector.getY();
+            t = p.getY() + p.getX()*m;
+        }
+
+        /**
+        * Ueberprüfen, ob Punkt p Oberhalb der Gerde liegt (also kleineres Y, da Ursprung oben, links)
+        *@author fseiffer
+        *@return Ob Punkt p oberhalb, der Geraden
+        */
+
+        public boolean istOberhalb(Point2D.Double p){
+            double y = m*p.getX()+t;
+            if(y > p.getY()){
+                return true;
+            } 
+            return false;
+        }
     }
 }
