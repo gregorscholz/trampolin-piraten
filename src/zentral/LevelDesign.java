49b010a195e342eed9355b1e4d44b0c9cca6e71b
fseiffer <ferdinand.seiffert@stud.htwk-leipzig.de>
2022-01-10 20:51:20 +0000

    Revert "Revert "Merge branch 'main' of https://gitlab.imn.htwk-leipzig.de/gscholz/trampolin-piraten""
    This reverts commit 2b65dad3af790ecc8d0a29f0707e0a6af344f0f6

--- src/zentral/LevelDesign.java
+++ src/zentral/LevelDesign.java
@@ -2,6 +2,7 @@ package zentral;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -15,7 +16,7 @@ import spielelemente.IFSpielelement;
  * @author Ines Rohrbach
  */
 public class LevelDesign {
-	private Set<IFSpielelement> alleElemente = new HashSet<IFSpielelement>();
+	private ArrayList<IFSpielelement> alleElemente = new ArrayList<IFSpielelement>();
 	
 	/**
 	 * Konstruktor fuer ein Leveldesign, welches je nach gewaehltem Level und Schwierigkeit das gewuenschte Level erstellt.
@@ -28,42 +29,42 @@ public class LevelDesign {
 	public LevelDesign(int design, int schwierigkeit) {	
 		switch(design) {
 			case 0: if(schwierigkeit == 0) {
-						leseDatei("a-1.txt");
+						leseDatei("level/a-1.txt");
 					} else if (schwierigkeit == 1) {
-						leseDatei("a-2.txt");
+						leseDatei("level/a-2.txt");
 					} else {
-						leseDatei("a-3.txt");
+						leseDatei("level/a-3.txt");
 					} break;
 			case 1: if(schwierigkeit == 0) {
-						leseDatei("b-1.txt");
+						leseDatei("level/b-1.txt");
 					} else if (schwierigkeit == 1) {
-						leseDatei("b-2.txt");
+						leseDatei("level/b-2.txt");
 					} else {
-						leseDatei("b-3.txt");
+						leseDatei("level/b-3.txt");
 					} break;
 			case 2: if(schwierigkeit == 0) {
-						leseDatei("c-1.txt");
+						leseDatei("level/c-1.txt");
 					} else if (schwierigkeit == 1) {
-						leseDatei("c-2.txt");
+						leseDatei("level/c-2.txt");
 					} else {
-						leseDatei("c-3.txt");
+						leseDatei("level/c-3.txt");
 					} break;
 			case 3: if(schwierigkeit == 0) {
-						leseDatei("d-1.txt");
+						leseDatei("level/d-1.txt");
 					} else if (schwierigkeit == 1) {
-						leseDatei("d-2.txt");
+						leseDatei("level/d-2.txt");
 					} else {
-						leseDatei("d-3.txt");
+						leseDatei("level/d-3.txt");
 					} break;
 			case 4: if(schwierigkeit == 0) {
-						leseDatei("e-1.txt");
+						leseDatei("level/e-1.txt");
 					} else if (schwierigkeit == 1) {
-						leseDatei("e-2.txt");
+						leseDatei("level/e-2.txt");
 					} else {
-						leseDatei("e-3.txt");
+						leseDatei("level/e-3.txt");
 					} break;
 			default: 
-				leseDatei("errorLevel.txt"); //man koennte ein extra leveldesign dafuer haben, damit fuer uns direkt klar ist das beim einlesen was nicht funktioniert, aber wir ein bisschen spa� haben koennen.
+				leseDatei("level/errorLevel.txt"); //man koennte ein extra leveldesign dafuer haben, damit fuer uns direkt klar ist das beim einlesen was nicht funktioniert, aber wir ein bisschen spa� haben koennen.
 				break;
 		}
 	}
@@ -75,25 +76,25 @@ public class LevelDesign {
 	 * @author Ines Rohrbach
 	 */
 	private void leseDatei(String dateiname) {	
-		File fileAlleStadt = new File(dateiname);
+		File datei = new File(dateiname);
 		Scanner scanner = null;
 		try {
-			scanner = new Scanner(fileAlleStadt);
+			scanner = new Scanner(datei);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			System.out.println("Datei lesen fehlerhaft!");
 		}while(scanner.hasNextLine()) {
 			alleElemente.add(LevelFactory.erstelleElement(scanner.nextLine()));
-		}		
+		}
 	}
 
 	/**
-	 * Gibt alle f�r das Level verwendeten Spielelemente zurueck
+	 * Gibt alle fuer das Level verwendeten Spielelemente zurueck
 	 * 
 	 * @return alle Spielelmente des Levels in einem Hashset
 	 * @author Ines Rohrbach
 	 */
-	public Set<IFSpielelement> getElemente() {
+	public ArrayList<IFSpielelement> getElemente() {
 		return alleElemente;
 	}
 }
