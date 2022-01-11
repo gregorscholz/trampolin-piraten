d4bd6a2f3190187c1a52d30059c5b674f29ca543
I <ines.rohrbach@tud.htwk-leipzig.de>
2022-01-11 23:53:42 +0100

    ... push wollte nicht

---
+++ src/zentral/TestController.java
@@ -0,0 +1,14 @@
+package zentral;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestController {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
