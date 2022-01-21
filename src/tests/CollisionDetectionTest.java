d6b10c150fc147e07a78c8aab12b52012641531a
fseiffer <ferdinand.seiffert@stud.htwk-leipzig.de>
2022-01-21 01:44:52 +0100

    CollisionDetectionTest mit kleinem Test

---
+++ src/tests/CollisionDetectionTest.java
@@ -0,0 +1,37 @@
+package tests;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runners.Parameterized.BeforeParam;
+
+import gui.InGamePanel;
+import spielelemente.*;
+
+public class CollisionDetectionTest {
+    CollisionDetection collisionDetection;
+    GameObjects gameObjects;
+
+    void setup1Kugel1Fass(int fassnummer, int kXOffset, int kYOffset, double vX, double vY){
+        ArrayList<IFSpielelement> elemente = new ArrayList<IFSpielelement>();
+        Fass f = new Fass(22, 1, 0);
+        Kugel k = new Kugel(f.getxKoordinate()-kXOffset, f.getyKoordinate()-kYOffset, 0);
+        k.setxVelocity(vX); k.setyVelocity(vY);
+        elemente.add(f); 
+        elemente.add(k);
+        gameObjects = new GameObjects(elemente);
+        collisionDetection = new CollisionDetection(gameObjects, new InGamePanel());
+    }
+
+    @Test
+    void testCheckCollision() {      
+        setup1Kugel1Fass(22, 2, 1, 1, 1);
+        collisionDetection.checkCollision();
+        assertTrue("Kollision mit Vector 1, 1 unterhalb",-1.0 == gameObjects.getKugel(0).getxVelocity());
+    }
+}
